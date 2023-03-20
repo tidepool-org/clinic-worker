99d8d1b03d4d4d725b24558f2d0bd80e0dc901ad
Alexander Diana <alex@rouk.org>
2023-03-20 01:36:26 +0000

    fix debug crashing

--- patientsummary/consumer.go
+++ patientsummary/consumer.go
@@ -89,7 +89,9 @@ func (p *CDCConsumer) handleMessage(cm *sarama.ConsumerMessage) error {
 		p.logger.Warnw("unable to unmarshal message", "offset", cm.Offset, zap.Error(err))
 		return err
 	}
-	p.logger.Debugw("handling document:", staticEvent)
+
+	p.logger.Debugw("event being processed", "event", staticEvent)
+
 	if staticEvent.FullDocument.Type == nil {
 		p.logger.Warnw("unable to get type of unmarshalled message", "offset", cm.Offset)
 		return errors.New("unable to get type of unmarshalled message, summary without type")
