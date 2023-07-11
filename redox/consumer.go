ba6667b99fee54517e98576732c1da137021f4b7
Todd Kazakov <todd.kazakov@gmail.com>
2023-07-11 15:38:04 +0300

    Add matching logic for orders

--- redox/consumer.go
+++ redox/consumer.go
@@ -1,7 +1,10 @@
 package redox
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/Shopify/sarama"
 	"github.com/tidepool-org/clinic-worker/cdc"
 	clinics "github.com/tidepool-org/clinic/client"
@@ -11,6 +14,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
+	"net/http"
 	"strconv"
 	"time"
 )
@@ -125,9 +129,81 @@ func (p *RedoxCDCConsumer) handleCDCEvent(event cdc.Event[models.MessageEnvelope
 				return err
 			}
 
-			p.logger.Debugw("successfully unmarshalled new order", "offset", event.Offset, "order", order)
+			p.logger.Debugw("successfully unmarshalled new order", "offset", event.Offset, "order", order.Meta)
+			return p.handleOrder(order)
 		}
 	}
+
+	return nil
+}
+
+func (p *RedoxCDCConsumer) handleOrder(order models.NewOrder) error {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	criteria := clinics.MatchClinicAndPatientJSONRequestBody{
+		Clinic: clinics.EHRMatchClinicRequest{
+			SourceId: *order.Meta.Source.ID,
+		},
+		Patient: &clinics.EHRMatchPatientRequest{},
+	}
+	if order.Order.OrderingFacility != nil && order.Order.OrderingFacility.Name != nil {
+		criteria.Clinic.FacilityName = order.Order.OrderingFacility.Name
+	}
+
+	for _, identifier := range order.Patient.Identifiers {
+		if identifier.IDType == "MR" {
+			criteria.Patient.Mrn = identifier.ID
+			break
+		}
+	}
+	if criteria.Patient.Mrn == "" {
+		p.logger.Warnw("unable to find MRN for order", "order", order.Meta)
+		return nil
+	}
+	if order.Patient.Demographics == nil {
+		p.logger.Warnw("unable to find patient demographics for order", "order", order.Meta)
+		return nil
+	}
+	if order.Patient.Demographics.DOB == nil || *order.Patient.Demographics.DOB == "" {
+		p.logger.Warnw("unable to find patient DOB for order", "order", order.Meta)
+		return nil
+	}
+	if order.Patient.Demographics.FirstName == nil || *order.Patient.Demographics.FirstName == "" {
+		p.logger.Warnw("unable to find patient first name for order", "order", order.Meta)
+		return nil
+	}
+	if order.Patient.Demographics.LastName == nil || *order.Patient.Demographics.LastName == "" {
+		p.logger.Warnw("unable to find patient last name for order", "order", order.Meta)
+		return nil
+	}
+
+	criteria.Patient.DateOfBirth = *order.Patient.Demographics.DOB
+	criteria.Patient.FirstName = order.Patient.Demographics.FirstName
+	criteria.Patient.LastName = order.Patient.Demographics.LastName
+	criteria.Patient.MiddleName = order.Patient.Demographics.MiddleName
+
+	response, err := p.clinics.MatchClinicAndPatientWithResponse(ctx, criteria)
+	if err != nil {
+		p.logger.Warnw("unable to match clinic and patient", "order", order.Meta, zap.Error(err))
+		return err
+	}
+	if response.StatusCode() != http.StatusOK {
+		p.logger.Warnw("unable to match clinic and patient", "order", order.Meta, "status", response.StatusCode())
+		return fmt.Errorf("unable to match clinic and patient. unexpected response: %d", response.StatusCode())
+	}
+	if response.JSON200 == nil {
+		return fmt.Errorf("unable to match clinic and patient: %d", errors.New("response body is nil"))
+	}
+	match := response.JSON200
+	if match.Patients == nil || len(*match.Patients) == 0 {
+		p.logger.Warnw("no matching patients were found", "order", order.Meta, "clinicId", match.Clinic.Id)
+	} else if len(*match.Patients) > 1 {
+		p.logger.Warnw(fmt.Sprintf("%v patients were found matching the order", len(*match.Patients)), "order", order.Meta, "clinicId", match.Clinic.Id)
+	}
+
+	patient := (*match.Patients)[0]
+	p.logger.Infow("successfully matched clinic and patient", "order", order.Meta, "clinicId", match.Clinic.Id, "patientId", patient.Id)
 
 	return nil
 }
