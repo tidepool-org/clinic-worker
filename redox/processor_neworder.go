cdb5da6a02386356f621cf749504be85b0493edc
Todd Kazakov <todd.kazakov@gmail.com>
2023-09-27 16:21:50 +0300

    Add ability to create patients accounts with an order

--- redox/processor_neworder.go
+++ redox/processor_neworder.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/deepmap/oapi-codegen/pkg/types"
 	"github.com/tidepool-org/clinic-worker/report"
 	clinics "github.com/tidepool-org/clinic/client"
 	models "github.com/tidepool-org/clinic/redox_models"
+	"github.com/tidepool-org/go-common/clients/shoreline"
+	"github.com/tidepool-org/go-common/clients/status"
 	"go.uber.org/zap"
 	"net/http"
+	"net/mail"
+	"strings"
 	"time"
 )
 
@@ -28,6 +33,7 @@ type newOrderProcessor struct {
 	clinics         clinics.ClientWithResponsesInterface
 	client          Client
 	reportGenerator report.Generator
+	shoreline       shoreline.Client
 }
 
 func NewNewOrderProcessor(clinics clinics.ClientWithResponsesInterface, redox Client, reportGenerator report.Generator, logger *zap.SugaredLogger) NewOrderProcessor {
@@ -68,14 +74,15 @@ func (o *newOrderProcessor) ProcessOrder(ctx context.Context, envelope models.Me
 	match := response.JSON200
 	procedureCode := GetProcedureCode(order)
 
-	switch procedureCode {
-	case match.Settings.ProcedureCodes.EnableSummaryReports:
+	if procedureCode == match.Settings.ProcedureCodes.EnableSummaryReports {
 		return o.handleEnableSummaryReports(ctx, envelope, order, *match)
-	case match.Settings.ProcedureCodes.DisableSummaryReports:
+	} else if procedureCode == match.Settings.ProcedureCodes.DisableSummaryReports {
 		return o.handleDisableSummaryReports(ctx, order, *match)
-	default:
-		return o.handleUnknownProcedure(ctx, order, *match)
+	} else if match.Settings.ProcedureCodes.CreateAccount != nil && *match.Settings.ProcedureCodes.CreateAccount == procedureCode {
+		return o.handleCreateAccount(ctx, order, *match)
 	}
+
+	return o.handleUnknownProcedure(ctx, order, *match)
 }
 
 func GetProcedureCode(order models.NewOrder) string {
@@ -112,6 +119,79 @@ func (o *newOrderProcessor) handleDisableSummaryReports(ctx context.Context, ord
 	patient := (*match.Patients)[0]
 	o.logger.Infow("successfully matched clinic and patient", "order", order.Meta, "clinicId", match.Clinic.Id, "patientId", patient.Id)
 	return o.handleSuccessfulPatientMatch(ctx, order, match)
+}
+
+func (o *newOrderProcessor) handleCreateAccount(ctx context.Context, order models.NewOrder, match clinics.EHRMatchResponse) error {
+	if match.Patients != nil && len(*match.Patients) > 0 {
+		err := fmt.Errorf("patient already exists")
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+
+	permission := make(map[string]interface{})
+	createPatient := clinics.CreatePatientAccountJSONRequestBody{
+		Permissions: &clinics.PatientPermissions{
+			Custodian: &permission,
+			Note:      &permission,
+			Upload:    &permission,
+			View:      &permission,
+		},
+	}
+
+	var err error
+	createPatient.Email, err = GetEmailAddressFromOrder(order)
+	if err != nil {
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+	createPatient.BirthDate, err = GetBirthDateFromOrder(order)
+	if err != nil {
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+	createPatient.FullName, err = GetFullNameFromOrder(order)
+	if err != nil {
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+	createPatient.Mrn, err = GetMrnFromOrder(order)
+	if err != nil {
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+
+	if exists, err := o.emailExists(*createPatient.Email); err != nil {
+		o.logger.Errorw("unexpected error when checking for duplicate emails", "order", order.Meta, "error", err)
+		return err
+	} else if exists {
+		err = fmt.Errorf("the email address is already in use")
+		return o.handleAccountCreationError(ctx, err, order, match)
+	}
+
+	resp, err := o.clinics.CreatePatientAccountWithResponse(ctx, *match.Clinic.Id, createPatient)
+	if err != nil {
+		// Retry in case of unexpected failure
+		o.logger.Errorw("unable to create patient account", "order", order.Meta, "error", err)
+		return err
+	}
+	if (resp.StatusCode() != http.StatusOK && resp.StatusCode() != http.StatusConflict) || resp.JSON200 == nil {
+		// Retry in case of failure
+		o.logger.Errorw("unexpected response when creating patient account", "order", order.Meta, "statusCode", resp.StatusCode())
+		return err
+	}
+
+	o.logger.Infow("patient account was successfully created", "order", order.Meta, "clinicId", match.Clinic.Id, "patientId", resp.JSON200.Id)
+	return o.handleAccountCreationSuccess(ctx, order, match)
+}
+
+func (o *newOrderProcessor) emailExists(email string) (bool, error) {
+	user, err := o.shoreline.GetUser(email, o.shoreline.TokenProvide())
+	if user != nil && len(user.UserID) > 0 {
+		return true, nil
+	}
+	if err != nil {
+		statusErr := &status.StatusError{}
+		if ok := errors.As(err, &statusErr); ok && statusErr.Code == http.StatusNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return false, nil
 }
 
 func (o *newOrderProcessor) SendSummaryAndReport(ctx context.Context, patient clinics.Patient, order models.NewOrder, match clinics.EHRMatchResponse) error {
@@ -246,7 +326,7 @@ func (o *newOrderProcessor) handleUnknownProcedure(ctx context.Context, order mo
 
 func (o *newOrderProcessor) handleNoMatchingPatients(ctx context.Context, order models.NewOrder, match clinics.EHRMatchResponse) error {
 	o.logger.Infow("No patients matched.", "order", order.Meta)
-	return o.sendMatchingResultsNotification(ctx, MatchingResult{
+	return o.sendMatchingResultsNotification(ctx, ResultsNotification{
 		IsSuccess: false,
 		Message:   NoMatchingPatientsMessage,
 	}, order, match)
@@ -254,7 +334,7 @@ func (o *newOrderProcessor) handleNoMatchingPatients(ctx context.Context, order 
 
 func (o *newOrderProcessor) handleMultipleMatchingPatients(ctx context.Context, order models.NewOrder, match clinics.EHRMatchResponse) error {
 	o.logger.Infow("Multiple patients matched.", "order", order.Meta)
-	return o.sendMatchingResultsNotification(ctx, MatchingResult{
+	return o.sendMatchingResultsNotification(ctx, ResultsNotification{
 		IsSuccess: false,
 		Message:   MultipleMatchingPatientsMessage,
 	}, order, match)
@@ -262,14 +342,30 @@ func (o *newOrderProcessor) handleMultipleMatchingPatients(ctx context.Context, 
 
 func (o *newOrderProcessor) handleSuccessfulPatientMatch(ctx context.Context, order models.NewOrder, match clinics.EHRMatchResponse) error {
 	o.logger.Infow("Found matching patient.", "order", order.Meta)
-	return o.sendMatchingResultsNotification(ctx, MatchingResult{
+	return o.sendMatchingResultsNotification(ctx, ResultsNotification{
 		IsSuccess: true,
 		Message:   SuccessfulMatchingMessage,
 	}, order, match)
 }
 
-func (o *newOrderProcessor) sendMatchingResultsNotification(ctx context.Context, matchingResult MatchingResult, order models.NewOrder, match clinics.EHRMatchResponse) error {
-	o.logger.Infow("Sending results notification", "order", order.Meta)
+func (o *newOrderProcessor) handleAccountCreationSuccess(ctx context.Context, order models.NewOrder, match clinics.EHRMatchResponse) error {
+	o.logger.Infow("account was successfully created", "order", order.Meta)
+	return o.sendAccountCreationResultsNotification(ctx, ResultsNotification{
+		IsSuccess: true,
+		Message:   SuccessfulAccountCreationMessage,
+	}, order, match)
+}
+
+func (o *newOrderProcessor) handleAccountCreationError(ctx context.Context, err error, order models.NewOrder, match clinics.EHRMatchResponse) error {
+	o.logger.Warnw("unable to create account", "order", order.Meta, "error", err)
+	return o.sendAccountCreationResultsNotification(ctx, ResultsNotification{
+		IsSuccess: false,
+		Message:   err.Error(),
+	}, order, match)
+}
+
+func (o *newOrderProcessor) sendMatchingResultsNotification(ctx context.Context, notification ResultsNotification, order models.NewOrder, match clinics.EHRMatchResponse) error {
+	o.logger.Infow("Sending matching results notification", "order", order.Meta)
 	source := o.client.GetSource()
 	destinationId := match.Settings.DestinationIds.Results
 	destinations := []struct {
@@ -283,7 +379,7 @@ func (o *newOrderProcessor) sendMatchingResultsNotification(ctx context.Context,
 	results.Meta.Source = &source
 	results.Meta.Destinations = &destinations
 	SetResultsPatientFromOrder(order, &results)
-	SetMatchingResult(matchingResult, order, &results)
+	SetMatchingResult(notification, order, &results)
 
 	if err := o.client.Send(ctx, results); err != nil {
 		// Return an error so we can retry the request
@@ -291,4 +387,93 @@ func (o *newOrderProcessor) sendMatchingResultsNotification(ctx context.Context,
 	}
 
 	return nil
+}
+
+func (o *newOrderProcessor) sendAccountCreationResultsNotification(ctx context.Context, notification ResultsNotification, order models.NewOrder, match clinics.EHRMatchResponse) error {
+	o.logger.Infow("Sending account creation results notification", "order", order.Meta)
+	source := o.client.GetSource()
+	destinationId := match.Settings.DestinationIds.Results
+	destinations := []struct {
+		ID   *string `json:"ID"`
+		Name *string `json:"Name"`
+	}{{
+		ID: &destinationId,
+	}}
+
+	results := NewResults()
+	results.Meta.Source = &source
+	results.Meta.Destinations = &destinations
+	SetResultsPatientFromOrder(order, &results)
+	SetAccountCreationResults(notification, order, &results)
+
+	if err := o.client.Send(ctx, results); err != nil {
+		// Return an error so we can retry the request
+		return fmt.Errorf("unable to send results: %w", err)
+	}
+
+	return nil
+}
+
+func GetBirthDateFromOrder(order models.NewOrder) (types.Date, error) {
+	if order.Patient.Demographics == nil || order.Patient.Demographics.DOB == nil {
+		return types.Date{}, fmt.Errorf("date of birth is missing")
+	}
+
+	birthDate := &types.Date{}
+	err := birthDate.UnmarshalText([]byte(*order.Patient.Demographics.DOB))
+	if err != nil {
+		return *birthDate, err
+	}
+	return *birthDate, nil
+}
+
+func GetEmailAddressFromOrder(order models.NewOrder) (*string, error) {
+	if order.Patient.Demographics == nil || order.Patient.Demographics.EmailAddresses == nil || len(*order.Patient.Demographics.EmailAddresses) == 0 {
+		return nil, fmt.Errorf("email address is missing")
+	}
+
+	email, ok := (*order.Patient.Demographics.EmailAddresses)[0].(string)
+	if !ok {
+		return nil, fmt.Errorf("email address is not a string")
+	}
+
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		return nil, fmt.Errorf("email address is invalid")
+	}
+
+	return &email, nil
+}
+
+func GetFullNameFromOrder(order models.NewOrder) (string, error) {
+	if order.Patient.Demographics == nil {
+		return "", fmt.Errorf("patient demographics is missing")
+	}
+	if order.Patient.Demographics.FirstName == nil || len(*order.Patient.Demographics.FirstName) == 0 {
+		return "", fmt.Errorf("first name is missing")
+	}
+	if order.Patient.Demographics.LastName == nil || len(*order.Patient.Demographics.LastName) == 0 {
+		return "", fmt.Errorf("last name is missing")
+	}
+	name := strings.Join([]string{*order.Patient.Demographics.FirstName, *order.Patient.Demographics.LastName}, " ")
+	return name, nil
+}
+
+func GetMrnFromOrder(order models.NewOrder) (*string, error) {
+	if len(order.Patient.Identifiers) == 0 {
+		return nil, fmt.Errorf("mrn is missing")
+	}
+	var mrn *string
+	for _, identifier := range order.Patient.Identifiers {
+		if strings.ToLower(identifier.IDType) == "mrn" {
+			mrn = &identifier.ID
+			break
+		}
+	}
+
+	if mrn == nil {
+		return nil, fmt.Errorf("mrn is missing")
+	}
+
+	return mrn, nil
 }
