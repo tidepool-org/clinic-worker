850b06c598d9797b3f590c9c16aaf7854f25c1fb
Todd Kazakov <todd.kazakov@gmail.com>
2024-08-28 10:55:33 +0300

    Allow unmarshalling ext json into custom cdc structs

--- cdc/mongo.go
+++ cdc/mongo.go
@@ -1,7 +1,22 @@
 package cdc
 
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type ObjectId struct {
 	Value string `json:"$oid"`
+}
+
+func (o *ObjectId) UnmarshalBSON(data []byte) error {
+	var id primitive.ObjectID
+	err := bson.Unmarshal(data, &id)
+	if err != nil {
+		return err
+	}
+	o.Value = id.Hex()
+	return nil
 }
 
 type Date struct {
